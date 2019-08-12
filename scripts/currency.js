48d79cea5ac5dd1e82a4f28e052f5e92196bb495
JuanJUribe <juanjouribev1@gmail.com>
2019-08-12 11:20:42 -0700

    Currency.js final hardcode

--- scripts/currency.js
+++ scripts/currency.js
@@ -5,11 +5,18 @@ const conversionCurrency = document.querySelector('#conversionCurrency');
 
 
 inputBox.addEventListener('input', conversion);
+initialCurrency.addEventListener('input', conversion);
+conversionCurrency.addEventListener('input', conversion);
 
 function conversion(){
   const initialValue = inputBox.value;
   const conversionValue = conversionCurrency.value/initialCurrency.value;
   const resultValue = initialValue*conversionValue;
 
-  conversionResult.textContent = resultValue;
+
+  if (isNaN(resultValue)) {
+    conversionResult.textContent = '?';
+  } else {
+    conversionResult.textContent = resultValue;
+  }
 }

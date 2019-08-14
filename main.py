791a7af716fcbdb7a7ec19ac620c7ec893a365ce
paumaya <paumaya2000mc@gmail.com>
2019-08-14 18:39:47 -0300

    Add contact to main page

--- main.py
+++ main.py
@@ -75,6 +75,14 @@ class FetchTranslationHandler(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(translation)
 
+class ContactHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/contact.html')
+        self.response.write(template.render())
+    def post(self):
+        print('big success')
+        self.response.write('hola')
+
 app = webapp2.WSGIApplication([
     ('/', MainPageHandler),
     ('/booking', BookingHandler),
@@ -84,4 +92,5 @@ app = webapp2.WSGIApplication([
     ('/fetchlocationweather/([\w %]*)', FetchWeatherLocationHandler),
     ('/fetchweather/(\d+)', FetchWeatherHandler),
     ('/fetchtranslate/([\w %]*)/(\w*)', FetchTranslationHandler),
+    ('/contact-us', ContactHandler),
     ], debug=True)
